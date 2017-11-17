48e03d98c50365e9d277307ca6229fcea364a898
jiangpeng <jiangpeng@houbank.com>
2017-11-18 01:33:22 +0800

    update jsonp

--- jsonp.js
+++ jsonp.js
@@ -1,13 +1,13 @@
-var jsonp = function (url, callback) {
+var jsonp = function (url, callback) { //参数为请求的URL地址，服务器生成的回调函数的名称（默认为callback）
     callback = callback || 'callback'
-    var id = new Date().getTime() + ((Math.random() + '').split('.').pop());
+    var id = new Date().getTime() + ((Math.random() + '').split('.').pop());//为回调函数添加随机ID
     return new Promise(function (resolve, reject) {
         window[callback + id] = function (data) {
             complete(data, null)
         }
         var script = document.createElement("script")
         if (window.addEventListener) {
-            script.addEventListener("error", function (err) {
+            script.addEventListener("error", function (err) { //如果是webkt内核的JSON请求出错，调用complete
                 complete(null, err)
             })
         } else {
@@ -17,7 +17,7 @@ var jsonp = function (url, callback) {
                     complete(null, err)
                 })
             } else {
-                script.onreadystatechange = function () {
+                script.onreadystatechange = function () { //IE6-8 Opera 不支持srcipt的onerrer事件，但是script会先执行加载的代码，再触发onreadystatechange事件，利用complete函数是否执行来归结错误
                     if (this.readyState == 'loaded' || this.readyState == 'complete') {
                         if (typeof window[callback + id] == "function") {
                             complete(null, {target: {src: url}})
@@ -26,7 +26,7 @@ var jsonp = function (url, callback) {
                 }
             }
         }
-        script.src = url + (url.indexOf("?") > 0 ? "&" : "?") + callback + "=" + callback + id;
+        script.src = url + (url.indexOf("?") > 0 ? "&" : "?") + callback + "=" + callback + id;//为url添加回调函数名
         document.getElementsByTagName("head")[0].appendChild(script)
         function complete(data, err) {
             if (!err) {
@@ -38,15 +38,10 @@ var jsonp = function (url, callback) {
             }
             script.parentNode.removeChild(script)
             try {
-                delete window[callback + id]
+                delete window[callback + id] //部分浏览器删除会Window上属性会有错误
             } catch (e) {
                 window[callback + id] = null
             }
         }
     })
 }
-jsonp("https://qaw.houbank.com/hxd/wechat/jsapi_cfg?&url=" + encodeURIComponent('https://hxd.houbank.com/hxd/client/index.html')).then(function (data) {
-    console.info(data, "succ")
-}, function (err) {
-    alert(err)
-})
